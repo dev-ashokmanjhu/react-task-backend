d90ed4d99bb552688c7a5d10c0b25d99d159eadd
ashokmanjhu <officialashokmanjhu@gmail.com>
2023-02-17 14:04:45 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+app.use(cors());
+
+const Users = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+
+    phone: "(254)954-1289",
+    website: "demarco.info",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.json("Server is running!!!");
+});
+
+app.get("/users", (req, res) => {
+  res.json(Users);
+});
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server running successfully on Port : ${port}`);
+});
